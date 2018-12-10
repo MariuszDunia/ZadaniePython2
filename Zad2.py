ce74fdb03008c505ef88c980c49bc6bf8249ed5b
MariuszDunia <45542293+MariuszDunia@users.noreply.github.com>
2018-12-10 16:35:41 +0100

    Add files via upload

--- Zad2.py
+++ Zad2.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 
-class Bath_bun():
+class Cake():
     pass
 
-bath_bun = Bath_bun()
+cake = Cake()
+
+print(cake)
+print(isinstance(cake, Cake))
+print(isinstance(cake, object))
+
+
+class BathBun(Cake):
+    pass
+
+bath_bun = BathBun()
 
 print(bath_bun)
-print(isinstance(bath_bun,Bath_bun))
+print(isinstance(bath_bun, BathBun))
 print(isinstance(bath_bun, Cake))
 print(isinstance(bath_bun, object))
 
-class Yeast(Bath_bun):
+class Yeast(BathBun):
     pass
 
-class Raisins(Bath_bun):
+class Raisin(BathBun):
     pass
 
 yeast = Yeast()
-raisins = Raisins()
+raisin = Raisin()
 
 print(yeast)
 print(isinstance(yeast, Yeast))
-print(isinstance(yeast, Bath_bun))
+print(isinstance(yeast, BathBun))
 print(isinstance(yeast, object))
 
-print(raisins)
-print(isinstance(raisins, Raisins))
-print(isinstance(raisins, Bath_bun))
-print(isinstance(raisins, object))
+print(raisin)
+print(isinstance(raisin, Raisin))
+print(isinstance(raisin, BathBun))
+print(isinstance(raisin, object))
 
-print(isinstance(raisins, Yeast))
-print(isinstance(yeast, Raisins))
+print(isinstance(raisin, Yeast))
+print(isinstance(yeast, Raisin))
